d875a779c776c7e644722ed682f5e173aeb12b48
AntonShishkin <nuegotonevzlomaut@gmail.com>
2021-03-08 01:23:18 +0300

    create quest_2.py

---
+++ quest_2.py
@@ -0,0 +1,21 @@
+my_list = [0, ]
+my_list.insert(0, input("Введите первый элемент: "))
+my_list.pop(1)
+n = 1
+while True:
+    q = input("Нужно большое элементов? Да - любое; Нет - 0. ")
+    if q == '0':
+        break
+    my_list.append(input("Введите элемент: "))
+    n += 1
+print(my_list)
+i = 0
+if n % 2 == 0:
+    while i < n - 1:
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+        i += 2
+else:
+    while i < n - 2:
+        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+        i += 2
+print(my_list)
